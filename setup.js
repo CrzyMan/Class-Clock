529efe375739ec6c0548e67a52339da5a0e74aa2
Tyler Whitehouse <tywhitehouse10@hotmail.com>
2015-03-31 01:12:29 -0400

    almost done fixing mobile sizing

--- setup.js
+++ setup.js
@@ -23,7 +23,7 @@ var MainEvent;
 
 function initialize(){
     if (isMobile)
-        document.getElementsByTagName("a")[0].style.fontSize = "15pt";
+        document.getElementsByTagName("a")[0].style.fontSize = "5pt";
 
     // make all of the class events, perhaps produce a data structure to load these from
     EventFactory.addEvent(8,5,"Start of First", false, document.getElementById("0s"));
